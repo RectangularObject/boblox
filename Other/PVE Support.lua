557a7ca1d98ef8d1a66183a6426f40d88bb359a9
Squares <carlosrosa071@gmail.com>
2022-02-08 22:04:57 -0500

    Update PVE Support.lua

--- Other/PVE Support.lua
+++ Other/PVE Support.lua
@@ -13,7 +13,6 @@ Unsupported Games: RECOIL, Rolling Thunder PVE
     Changelogs
     2/8/2022 -- Squares
         [!] Fixed not being able to damage npcs after running this script
-            - Does not fix Rolling Thunder PVE's npcs, they have a weird system
     11/3/2021 -- Squares
         [*] Replaced all occurrences of "next, v" with "ipairs()"
         [*] Replaced depreciated function "Remove()" with "Destroy()"
