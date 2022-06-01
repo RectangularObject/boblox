c919a23fc7ed194387a2fcd1822ad70ee0e71ffd
Squares <carlosrosa071@gmail.com>
2022-05-31 21:15:16 -0400

    stylua'ed this hoe because it looked fucked

--- Other/Universal Hitbox Extender.lua
+++ Other/Universal Hitbox Extender.lua
@@ -1,16 +1,20 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
 
 -- thanks 3ds and kiko metatables r hard (https://v3rmillion.net/showthread.php?tid=1089069)
 -- my version uses hookmetamethod :D
-if not getgenv().MTAPIMutex then loadstring(game:HttpGet("https://raw.githubusercontent.com/RectangularObject/MT-Api-vhookmetamethod/main/__source/mt-api%20v2.lua", true))() end
+if not getgenv().MTAPIMutex then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RectangularObject/MT-Api-vhookmetamethod/main/__source/mt-api%20v2.lua", true))()
+end
 -- thanks lego hacker I love you for making this (https://v3rmillion.net/showthread.php?tid=1140873)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoHacker1337/legohacks/main/PhysicsServiceOnClient.lua"))()
 -- thanks Iris (https://v3rmillion.net/showthread.php?pid=8154179)
 --loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisInstanceProtect.lua"))()
-local Plrs = game:GetService'Players'
+local Plrs = game:GetService("Players")
 local lPlayer = Plrs.LocalPlayer or Plrs.PlayerAdded:Wait()
 local physService = game:GetService("PhysicsService")
-local RunService = game:GetService'RunService'
+local RunService = game:GetService("RunService")
 local collisiongroups = physService:GetCollisionGroups()
 local collisionsready = false
 
@@ -24,9 +28,9 @@ task.spawn(function()
 	local character = lPlayer.Character or lPlayer.CharacterAdded:Wait()
 	local head = character:WaitForChild("Head")
 	local collisiongroup = physService:GetCollisionGroupName(head.CollisionGroupId)
-	for _,v in pairs(collisiongroups) do
+	for _, v in pairs(collisiongroups) do
 		if v.name == collisiongroup then
-			for _,b in pairs(collisiongroups) do
+			for _, b in pairs(collisiongroups) do
 				local result = physService:CollisionGroupsAreCollidable(v.name, b.name)
 				if not result then
 					disableCollisions(b.name)
@@ -43,20 +47,21 @@ if game.PlaceId == 111311599 then -- Critical Strike
 	-- I literally copied the rest of this from the "Serverbased Anticheat"
 	local sValue = game:GetService("Players").LocalPlayer:WaitForChild("SValue")
 	local function constructAnticheatString()
-		return "CS-" .. math.random(11111, 99999) .. "-" .. math.random(1111, 9999) .. "-" .. math.random(111111, 999999) .. math.random(1111111, 9999999) .. (sValue.Value * 6) ^ 2 + 18;
+		return "CS-" .. math.random(11111, 99999) .. "-" .. math.random(1111, 9999) .. "-" .. math.random(111111, 999999) .. math.random(1111111, 9999999) .. (sValue.Value * 6) ^ 2 + 18
 	end
 	-- to be fair the game hasn't been updated in over a year
 	task.spawn(function()
-		while true do task.wait(2)
-			game:GetService("ReplicatedStorage").ACDetect:FireServer(sValue.Value, constructAnticheatString());
+		while true do
+			task.wait(2)
+			game:GetService("ReplicatedStorage").ACDetect:FireServer(sValue.Value, constructAnticheatString())
 		end
 	end)
 	anticheat.Disabled = true
 end
 
 -- thanks inori and wally
-local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/Library.lua'))()
-local SaveManager = loadstring(game:HttpGet('https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/addons/SaveManager.lua'))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/Library.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/addons/SaveManager.lua"))()
 SaveManager:SetLibrary(Library)
 SaveManager:SetFolder("HitboxExtender")
 
@@ -67,52 +72,57 @@ local ignoresGroupbox = mainTab:AddRightGroupbox("Ignores")
 local espGroupbox = mainTab:AddLeftGroupbox("ESP")
 local miscGroupbox = mainTab:AddLeftGroupbox("Misc")
 
-local extenderToggled = mainGroupbox:AddToggle("extenderToggled", {Text = "Toggle"})
-local extenderSize = mainGroupbox:AddSlider("extenderSize", {Text = "Size", Min = 2, Max = 50, Default = 10, Rounding = 1})
-local extenderTransparency = mainGroupbox:AddSlider("extenderTransparency", {Text = "Transparency", Min = 0, Max = 1, Default = 0.5, Rounding = 2})
-local customPartNameInput = mainGroupbox:AddInput("customPartList", {Text = "Custom Part Name", Default = "HeadHB"})
-local extenderPartList = mainGroupbox:AddDropdown("extenderPartList", {Text = "Body Parts", AllowNull = true, Multi = true, Values = {"Custom Part", "Head", "HumanoidRootPart", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg"}, Default = "Head"})
-local extenderUpdateRate = miscGroupbox:AddSlider("extenderUpdateRate", {Text = "Update Rate", Min = 0, Max = 1000, Default = 0, Rounding = 0, Suffix = "ms"})
+local extenderToggled = mainGroupbox:AddToggle("extenderToggled", { Text = "Toggle" })
+local extenderSize = mainGroupbox:AddSlider("extenderSize", { Text = "Size", Min = 2, Max = 50, Default = 10, Rounding = 1 })
+local extenderTransparency = mainGroupbox:AddSlider("extenderTransparency", { Text = "Transparency", Min = 0, Max = 1, Default = 0.5, Rounding = 2 })
+local customPartNameInput = mainGroupbox:AddInput("customPartList", { Text = "Custom Part Name", Default = "HeadHB" })
+local extenderPartList = mainGroupbox:AddDropdown("extenderPartList", { Text = "Body Parts", AllowNull = true, Multi = true, Values = { "Custom Part", "Head", "HumanoidRootPart", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg" }, Default = "Head" })
+local extenderUpdateRate = miscGroupbox:AddSlider("extenderUpdateRate", { Text = "Update Rate", Min = 0, Max = 1000, Default = 0, Rounding = 0, Suffix = "ms" })
 
-local espNameToggled = espGroupbox:AddToggle("espNameToggled", {Text = "Name"})
-:AddColorPicker("espNameColor1", {Title = "Fill Color", Default = Color3.fromRGB(255, 255, 255)})
-:AddColorPicker("espNameColor2", {Title = "Outline Color", Default = Color3.fromRGB(0, 0, 0)})
-local espNameUseTeamColor = espGroupbox:AddToggle("espNameUseTeamColor", {Text = "Use Team Color For Name"})
-local espNameType = espGroupbox:AddDropdown("espNameType", {Text = "Name Type", AllowNull = false, Multi = false, Values = {"Display Name", "Account Name"}, Default = "Display Name"})
-local espHighlightToggled = espGroupbox:AddToggle("espHighlightToggled", {Text = "Chams"})
-:AddColorPicker("espHighlightColor1", {Title = "Fill Color", Default = Color3.fromRGB(0,0,0)})
-:AddColorPicker("espHighlightColor2", {Title = "Outline Color", Default = Color3.fromRGB(0, 0, 0)})
-local espHighlightUseTeamColor = espGroupbox:AddToggle("espHighlightUseTeamColor", {Text = "Use Team Color For Chams"})
-local espHighlightDepthMode = espGroupbox:AddDropdown("espHighlightDepthMode", {Text = "Chams Depth Mode", AllowNull = false, Multi = false, Values = {"Occluded", "AlwaysOnTop"}, Default = "Occluded"})
-local espHighlightFillTransparency = espGroupbox:AddSlider("espHighlightFillTransparency", {Text = "Chams Fill Transparency", Min = 0, Max = 1, Default = 0.5, Rounding = 2})
-local espHighlightOutlineTransparency = espGroupbox:AddSlider("espHighlightOutlineTransparency", {Text = "Chams Outline Transparency", Min = 0, Max = 1, Default = 0, Rounding = 2})
+local espNameToggled = espGroupbox:AddToggle("espNameToggled", { Text = "Name" }):AddColorPicker("espNameColor1", { Title = "Fill Color", Default = Color3.fromRGB(255, 255, 255) }):AddColorPicker("espNameColor2", { Title = "Outline Color", Default = Color3.fromRGB(0, 0, 0) })
+local espNameUseTeamColor = espGroupbox:AddToggle("espNameUseTeamColor", { Text = "Use Team Color For Name" })
+local espNameType = espGroupbox:AddDropdown("espNameType", { Text = "Name Type", AllowNull = false, Multi = false, Values = { "Display Name", "Account Name" }, Default = "Display Name" })
+local espHighlightToggled = espGroupbox:AddToggle("espHighlightToggled", { Text = "Chams" }):AddColorPicker("espHighlightColor1", { Title = "Fill Color", Default = Color3.fromRGB(0, 0, 0) }):AddColorPicker("espHighlightColor2", { Title = "Outline Color", Default = Color3.fromRGB(0, 0, 0) })
+local espHighlightUseTeamColor = espGroupbox:AddToggle("espHighlightUseTeamColor", { Text = "Use Team Color For Chams" })
+local espHighlightDepthMode = espGroupbox:AddDropdown("espHighlightDepthMode", { Text = "Chams Depth Mode", AllowNull = false, Multi = false, Values = { "Occluded", "AlwaysOnTop" }, Default = "Occluded" })
+local espHighlightFillTransparency = espGroupbox:AddSlider("espHighlightFillTransparency", { Text = "Chams Fill Transparency", Min = 0, Max = 1, Default = 0.5, Rounding = 2 })
+local espHighlightOutlineTransparency = espGroupbox:AddSlider("espHighlightOutlineTransparency", { Text = "Chams Outline Transparency", Min = 0, Max = 1, Default = 0, Rounding = 2 })
 
 local playerNames = {}
 --local npcNames = {} -- I was planning on adding npc support
 local teamNames = {}
 
-local extenderSitCheck = ignoresGroupbox:AddToggle("extenderSitCheck", {Text = "Ignore Sitting Players"})
-local extenderFFCheck = ignoresGroupbox:AddToggle("extenderFFCheck", {Text = "Ignore Forcefielded Players"})
-local ignoreSelectedPlayersToggled = ignoresGroupbox:AddToggle("ignoreSelectedPlayersToggled", {Text = "Ignore Selected Players"})
-local ignorePlayerList = ignoresGroupbox:AddDropdown("ignorePlayerList", {Text = "Players", AllowNull = true, Multi = true, Values = playerNames})
+local extenderSitCheck = ignoresGroupbox:AddToggle("extenderSitCheck", { Text = "Ignore Sitting Players" })
+local extenderFFCheck = ignoresGroupbox:AddToggle("extenderFFCheck", { Text = "Ignore Forcefielded Players" })
+local ignoreSelectedPlayersToggled = ignoresGroupbox:AddToggle("ignoreSelectedPlayersToggled", { Text = "Ignore Selected Players" })
+local ignorePlayerList = ignoresGroupbox:AddDropdown("ignorePlayerList", { Text = "Players", AllowNull = true, Multi = true, Values = playerNames })
 --local ignoreSelectedNpcsToggled = ignoresGroupbox:AddToggle("ignoreSelectedNpcsToggled", {Text = "Ignore Selected Npcs"})
 --local ignoreNpcList = ignoresGroupbox:AddDropdown("ignoreNpcList", {Text = "Npcs", AllowNull = true, Multi = true, Values = npcNames})
-local ignoreSelfTeamToggled = ignoresGroupbox:AddToggle("ignoreSelfTeamToggled", {Text = "Ignore Own Team"})
-local ignoreSelectedTeamsToggled = ignoresGroupbox:AddToggle("ignoreSelectedTeamsToggled", {Text = "Ignore Selected Teams"})
-local ignoreTeamList = ignoresGroupbox:AddDropdown("ignoreTeamList", {Text = "Teams", AllowNull = true, Multi = true, Values = teamNames})
+local ignoreSelfTeamToggled = ignoresGroupbox:AddToggle("ignoreSelfTeamToggled", { Text = "Ignore Own Team" })
+local ignoreSelectedTeamsToggled = ignoresGroupbox:AddToggle("ignoreSelectedTeamsToggled", { Text = "Ignore Selected Teams" })
+local ignoreTeamList = ignoresGroupbox:AddDropdown("ignoreTeamList", { Text = "Teams", AllowNull = true, Multi = true, Values = teamNames })
 
 -- thanks roblox dev forum
-local function CheckTableEquality(t1,t2)
-	for i,v in next, t1 do if t2[i]~=v then return false end end
-	for i,v in next, t2 do if t1[i]~=v then return false end end
+local function CheckTableEquality(t1, t2)
+	for i, v in next, t1 do
+		if t2[i] ~= v then
+			return false
+		end
+	end
+	for i, v in next, t2 do
+		if t1[i] ~= v then
+			return false
+		end
+	end
 	return true
 end
 
 -- updates the player list
 task.spawn(function()
-	while true do task.wait() -- if you cry about while true do loops then kys
+	while true do
+		task.wait() -- if you cry about while true do loops then kys
 		local temp = {}
-		for i,v in ipairs(Plrs:GetPlayers()) do
+		for i, v in ipairs(Plrs:GetPlayers()) do
 			if v ~= lPlayer then
 				temp[i] = v.Name
 			end
@@ -128,10 +138,11 @@ end)
 
 -- updates the team list
 task.spawn(function()
-	local Teams = game:GetService'Teams'
-	while true do task.wait()
+	local Teams = game:GetService("Teams")
+	while true do
+		task.wait()
 		local temp = {}
-		for i,v in pairs(Teams:GetTeams()) do
+		for i, v in pairs(Teams:GetTeams()) do
 			temp[i] = v.Name
 		end
 		if not CheckTableEquality(teamNames, temp) then
@@ -163,7 +174,12 @@ Library:Notify("hai :3")
 Library:Notify("Press right ctrl to open the menu")
 
 local function WaitForChildWhichIsA(parent, name)
-	while not parent:FindFirstChildWhichIsA(name) do task.wait() if parent == nil then return nil end end
+	while not parent:FindFirstChildWhichIsA(name) do
+		task.wait()
+		if parent == nil then
+			return nil
+		end
+	end
 	return parent:FindFirstChildWhichIsA(name)
 end
 
@@ -177,10 +193,12 @@ local function getBodyParts(character)
 		["Left Arm"] = {},
 		["Right Arm"] = {},
 		["Left Leg"] = {},
-		["Right Leg"] = {}
+		["Right Leg"] = {},
 	}
-	if parts.Humanoid == nil then return nil end
-	for _,v in pairs(character:GetChildren()) do
+	if parts.Humanoid == nil then
+		return nil
+	end
+	for _, v in pairs(character:GetChildren()) do
 		if v:IsA("BasePart") and parts.Humanoid:GetLimb(v) ~= Enum.Limb.Unknown then
 			if string.match(v.Name, "Torso") then
 				parts.Torso[v.Name] = v
@@ -216,7 +234,9 @@ local function addCharacter(character)
 	local originals = {}
 	local bodyParts = getBodyParts(character)
 	local player = Plrs:GetPlayerFromCharacter(character)
-	if bodyParts == nil or player == nil then return end
+	if bodyParts == nil or player == nil then
+		return
+	end
 	-- Sets up original sizes and creates hooks to bypass localscript anticheats
 	local function setup(i, v)
 		if not originals[i] then
@@ -266,20 +286,20 @@ local function addCharacter(character)
 				physService:SetPartCollisionGroup(customPart, originals[customPart.Name].CollisionGroup)
 			end
 		end
-		for i,v in pairs(bodyParts) do
+		for i, v in pairs(bodyParts) do
 			if string.lower(part) == string.lower(i) or part == "all" then
 				if i ~= "Humanoid" and type(v) ~= "table" then
 					if not originals[i] then
-						setup(i,v)
+						setup(i, v)
 					end
 					v.Size = originals[i].Size
 					v.Transparency = originals[i].Transparency
 					v.Massless = originals[i].Massless
 					physService:SetPartCollisionGroup(v, originals[i].CollisionGroup)
 				elseif type(v) == "table" then
-					for o,b in pairs(v) do
+					for o, b in pairs(v) do
 						if not originals[o] then
-							setup(o,b)
+							setup(o, b)
 						end
 						b.Size = originals[o].Size
 						b.Transparency = originals[o].Transparency
@@ -372,7 +392,9 @@ local function addCharacter(character)
 		return 0
 	end
 	-- loops and stuff
-	local nameEsp = Drawing.new("Text"); nameEsp.Center = true; nameEsp.Outline = true
+	local nameEsp = Drawing.new("Text")
+	nameEsp.Center = true
+	nameEsp.Outline = true
 	local RenderStepped
 	RenderStepped = RunService.RenderStepped:Connect(function()
 		if espNameToggled.Value then
@@ -417,21 +439,21 @@ local function addCharacter(character)
 		local checks = getChecks()
 		if timer >= (extenderUpdateRate.Value / 1000) then -- divided by 1000 because milliseconds
 			timer = 0
-            if espHighlightToggled.Value and checks == 0 then
-                if espHighlightUseTeamColor.Value then
-                    chams.FillColor = player.TeamColor.Color
-                    chams.OutlineColor = player.TeamColor.Color
-                else
-                    chams.FillColor = Options.espHighlightColor1.Value
-                    chams.OutlineColor = Options.espHighlightColor2.Value
-                end
-                chams.DepthMode = Enum.HighlightDepthMode[espHighlightDepthMode.Value]
-                chams.FillTransparency = espHighlightFillTransparency.Value
-                chams.OutlineTransparency = espHighlightOutlineTransparency.Value
-                chams.Enabled = true
-            else
-                chams.Enabled = false
-            end
+			if espHighlightToggled.Value and checks == 0 then
+				if espHighlightUseTeamColor.Value then
+					chams.FillColor = player.TeamColor.Color
+					chams.OutlineColor = player.TeamColor.Color
+				else
+					chams.FillColor = Options.espHighlightColor1.Value
+					chams.OutlineColor = Options.espHighlightColor2.Value
+				end
+				chams.DepthMode = Enum.HighlightDepthMode[espHighlightDepthMode.Value]
+				chams.FillTransparency = espHighlightFillTransparency.Value
+				chams.OutlineTransparency = espHighlightOutlineTransparency.Value
+				chams.Enabled = true
+			else
+				chams.Enabled = false
+			end
 			local bodyPartList = extenderPartList:GetActiveValues()
 			if checks == 2 then
 				reset("all")
@@ -460,11 +482,11 @@ local function addCharacter(character)
 				else
 					reset("custompart")
 				end
-				for i,v in pairs(bodyParts) do
+				for i, v in pairs(bodyParts) do
 					if table.find(bodyPartList, i) then
 						if type(v) ~= "table" then
 							if not originals[i] then
-								setup(i,v)
+								setup(i, v)
 							end
 							if i ~= "HumanoidRootPart" then
 								v.Massless = true
@@ -473,9 +495,9 @@ local function addCharacter(character)
 							v.Size = Vector3.new(extenderSize.Value, extenderSize.Value, extenderSize.Value)
 							v.Transparency = extenderTransparency.Value
 						else
-							for o,b in pairs(v) do
+							for o, b in pairs(v) do
 								if not originals[o] then
-									setup(o,b)
+									setup(o, b)
 								end
 								b.Massless = true
 								physService:SetPartCollisionGroup(b, "squarehookhackcheatexploit")
@@ -506,7 +528,7 @@ local function addCharacter(character)
 		end
 	end)
 end
-for _,player in ipairs(Plrs:GetPlayers()) do
+for _, player in ipairs(Plrs:GetPlayers()) do
 	if player ~= lPlayer then
 		player.CharacterAdded:Connect(addCharacter)
 		local Char = player.Character
@@ -516,12 +538,14 @@ for _,player in ipairs(Plrs:GetPlayers()) do
 	else
 		local function onDescendantAdded(descendant)
 			if descendant:IsA("BasePart") then
-				while not collisionsready do task.wait() end
+				while not collisionsready do
+					task.wait()
+				end
 				physService:SetPartCollisionGroup(descendant, "squarehookhackcheatexploit")
 			end
 		end
 		local function onCharacterAdded(character)
-			for _,v in pairs(character:GetDescendants()) do
+			for _, v in pairs(character:GetDescendants()) do
 				onDescendantAdded(v)
 			end
 			character.DescendantAdded:Connect(onDescendantAdded)
